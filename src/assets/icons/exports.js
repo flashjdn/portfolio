795ef2da801f577b9058043beccce24f3fc97984
Jordan Flash <flash.jordan.3@gmail.com>
2022-11-19 00:09:21 +0000

    pushing all assets

---
+++ src/assets/icons/exports.js
@@ -0,0 +1,20 @@
+import css from './css.svg'
+import aws from './aws.png'
+import figma from './figma.svg'
+import js from './js.svg'
+import git from './git.png'
+import github from './github.png'
+import heroku from './heroku.png'
+import html from './html5.svg'
+import jest from './jest.png'
+import netlify from './netlify.svg'
+import node from './nodejs.svg'
+import postgres from './postgres.png'
+import postman from './postman.png'
+import react from './reactjs.svg'
+import vs from './vs.svg'
+
+export { css, aws, figma, js, git , github, heroku, html, jest, netlify, node, postgres, postman, react, vs } 
+
+
+
---
+++ src/assets/images/jpeg/exports.js
@@ -0,0 +1,36 @@
+import bunkers from "./bunkers.jpg";
+import canadaWater from "./canadaWater.jpg"
+import cityHall from "./cityHall.jpg"
+import eye from "./eye.jpg"
+import famimart from "./famimart.jpg"
+import greece from "./greece.jpg"
+import lloyds from "./lloyds.jpg"
+import mediterranian from "./mediterranian.jpg"
+import pink from "./pink.jpg"
+import poseidon from "./poseidon.jpg"
+import sunrise from "./sunrise.jpg"
+import sunset from "./sunsetShard.jpg"
+import thames from "./thames.jpg"
+import tower from "./tower.jpg"
+import trafalgar from "./trafalgar.jpg"
+
+let images = [
+    bunkers,
+    canadaWater,
+    // cityHall,
+    eye,
+    famimart,
+    greece,
+    lloyds,
+    mediterranian,
+    pink,
+    poseidon,
+    sunrise,
+    // sunset,
+    thames,
+    // tower,
+    trafalgar
+]
+
+
+export default images 
