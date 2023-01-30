29adf422e64f2d02bbc5bee83f1496ab5acb0c6f
Jordan Flash <flash.jordan.3@gmail.com>
2023-01-30 23:51:42 +0000

    css changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <main>
       <Slideshow />
-      <Header />
+      {/* <Header /> */}
       <Profile />
       <Projects />
       <Footer />
---
+++ src/models/text.js
@@ -0,0 +1,15 @@
+let profile = {
+    intro: `I find talking about myself kinda cringe but I guess it’s necessary info for a portfolio. Let’s make it brief.`,
+    background: `Biomedical Sciences graduate from Uni of Southampton
+    Experience in Event Organisation and Quality Assurance before coming into the world of tech
+    Enrolled in a bootcamp April 2022 learning Web Dev with JavaScript`,
+    hobbies: `Rollerblading: I can go backwards and somehow not fall over (this is a lie sometimes I do fall over)
+    Cycling: (not the spandex road bike type, although I’m sure it’s very aerodynamic)
+    Incredibly amateur photography: The slideshow on this website is comprised of photos all taken by me
+    Video games: Currently playing Destiny 2 (this is a cry for help)`,
+    faq: `Q: Is your last name really Flash?
+    A: It actually is, BUT I’ve never watched Flash Gordon, nor am I an athletic prodigy so please don’t expect me to win at any sporting event.
+    Side note: CSS is currently undergoing reconstruction, and I'm in the process of moving over my project databases to MongoDB because Heroku wanted to be rude and remove their free tier.`
+}
+
+export default profile
