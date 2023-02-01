42f038e6a0c5c7a427c8d9d2107669d8ff408d89
Jordan Flash <flash.jordan.3@gmail.com>
2023-02-01 17:23:19 +0000

    font change to Monserrat and redesign of profile section as per figma wireframe

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -7,13 +7,51 @@ import avatar from '../../assets/images/profile-pic.jpg'
 
 const Profile = () => {
   return (
-    <section>
+    <section className="container">
       <h1> Jordan Flash</h1>
-      <h3>Software engineer</h3>
-      <div className="underline">underline here</div>
+      <h3>Junior Software Engineer</h3>
+      <div className="underline"></div>
       <div className="profile">
-        <h2>Background</h2>
-        <p>{profile.background}</p>
+        <div className="text">
+          <h2>Background</h2>
+          <p>Biomedical Sciences graduate from Uni of Southampton</p>
+          <p>
+            Experience in Event Organisation and Quality Assurance before coming
+            into the world of tech
+          </p>
+          <p>
+            Enrolled in a bootcamp April 2022 learning Web Dev with JavaScript
+          </p>
+          <h2>Interests and Hobbies</h2>
+          <p>
+            {' '}
+            Rollerblading: I can go backwards and somehow not fall over (this is
+            a lie sometimes I do fall over){' '}
+          </p>
+          <p>
+            {' '}
+            Cycling: (not the spandex road bike type, although I’m sure it’s
+            very aerodynamic){' '}
+          </p>
+          <p>
+            {' '}
+            Incredibly amateur photography: The slideshow on this website is
+            comprised of photos all taken by me{' '}
+          </p>
+          <p>
+            {' '}
+            Video games: Currently playing Destiny 2 (this is a cry for help){' '}
+          </p>
+          <h2> FAQ </h2>
+          <p> Q: Is your last name really Flash?</p>
+          <ul>
+            A: It actually is, BUT I’ve never watched Flash Gordon, nor am I an
+            athletic prodigy so please don’t expect me to win at any sporting
+            event.
+          </ul>
+        </div>
+          <img className='photo' src={avatar} alt="A headshot of the page's author, Jordan Flash"></img>
+
       </div>
     </section>
   )
